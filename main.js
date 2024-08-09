3751cb7268c966d001c3993ae75e4f8ba24891b7
Starist <codemitstarist@gmail.com>
2024-08-10 00:16:59 +0100

    editing clicking boxes

--- main.js
+++ main.js
@@ -10,33 +10,31 @@ addArray.forEach(ele => {
         card[3].style.display = 'flex';
         card[4].style.display = 'flex';
         card[5].style.display = 'block'; 
-        let newOrder = document.createElement("div")
-        newOrder.className = "newOrder";
-        let h2 = document.createElement("h2");
-        h2.innerHTML = this.parentElement.childNodes[5].innerHTML;
-        newOrder.appendChild(h2);
-        let desc = document.createElement("div");
-        desc.className = 'desc';
-        let first = document.createElement('span');
-        first.innerHTML = `${this.previousElementSibling.children[1].innerHTML}x`; first.className = 'first';
-        let second = document.createElement('span');
-        second.innerHTML = '$7'; second.className = 'second';
-        let third = document.createElement('span');
-        third.innerHTML = '$7'; third.className = 'third';
-        desc.appendChild(first);desc.appendChild(second);desc.appendChild(third);
-        newOrder.appendChild(desc);
-        ordersParent.appendChild(newOrder);
         this.style.display = "none";
         this.parentElement.firstElementChild.style.border = "solid 2px var(--Red)";
     })  
 })
 
+for(let i = 0; i<9; i++) {
+    addArray[i].onclick = () => {
+        ordersParent.children[i].style.display = "none";
+    }
+}
+
 let incre = document.querySelectorAll(".increment");
 let decre = document.querySelectorAll(".decrement");
+let card = document.querySelector("yourCard");
+let firsts = document.getElementsByClassName("first");
+let firstsArr = Array.from(firsts);
 
 incre.forEach(ele => {
     ele.onclick = function(ele) {
         this.previousElementSibling.innerHTML++;
+        for(let i = 0; i<add.length; i++) {
+            if (incre[i].parentElement.parentElement.children[2].innerHTML == firstsArr[i].innerHTML) {
+                console.log("true")
+            }
+        }
 }})
 
 decre.forEach(ele => {
